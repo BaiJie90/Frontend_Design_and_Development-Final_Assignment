2f64007408b79808a34cecef3a58dc971263a15d
Bai Jie Cao <bjcao@student.che.nl>
2021-10-17 02:10:50 +0200

    Test script voor uitklapbare menu toegevoegd.

---
+++ JavaScript/Dropdownscript.js
@@ -0,0 +1,19 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function Dropdown() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('L_MK')) {
+      var dropdowns = document.getElementsByClassName("L_DPMK");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
